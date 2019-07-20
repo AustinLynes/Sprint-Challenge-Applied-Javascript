8b2a9686855273f8614188ad2c47caded6a6d89d
AustinLynes <austinlynes94@gmail.com>
2019-07-19 20:49:19 -0700

    finished Cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -16,4 +16,77 @@
 //   </div>
 // </div>
 //
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then((_response) => {
+        console.log(_response);
+        const __cards = new Cards(_response);
+
+
+    })
+    .catch((_err) => {
+        console.error(_err);
+    })
+
+class Cards {
+    constructor(_response) {
+        this.__articles = _response.data.articles;
+        this.__container = document.querySelector('.cards-container');
+
+        console.log(this.__articles);
+
+        for (var article in this.__articles) {
+            for(var children in this.__articles[article]){
+            this.__headline = this.__articles[article][children].headline;
+            this.__image = this.__articles[article][children].authorPhoto;
+            this.__name = this.__articles[article][children].authorName;
+
+            this.createCard();
+
+            this.createHeadline(this.__headline);
+    
+            this.createImage(this.__image);
+    
+            this.createAuthor(this.__name);
+            }
+        }
+    }
+    createCard() {
+        this.__card = document.createElement('div');
+        this.__card.classList.add('card');
+        this.__container.appendChild(this.__card);
+    }
+    createHeadline(_hl) {
+        this.__headline = document.createElement('div');
+        this.__headline.classList.add('headline');
+        this.__headline.innerHTML = _hl;
+        this.__card.appendChild(this.__headline);
+    }
+    createImage(_src) {
+        this.__img_container = document.createElement('div');
+        this.__img_container.classList.add('img-container');
+        this.__card.appendChild(this.__img_container);
+
+        this.__img = document.createElement('img');
+        this.__img.src = _src;
+        this.__img_container.appendChild(this.__img);
+
+    }
+    createAuthor(_author) {
+        this.__author = document.createElement('span');
+        this.__author.innerHTML = _author;
+        this.__card.appendChild(this.__author);
+    }
+}
+
 // Create a card for each of the articles and add the card to the DOM.
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {authors name}</span>
+//   </div>
+// </div>
