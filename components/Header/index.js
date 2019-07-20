0670daa42b2648ccbea943319c985246a339ee9a
AustinLynes <austinlynes94@gmail.com>
2019-07-19 18:30:09 -0700

    finished header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+class Header{
+    constructor(){
+        this.__container = document.querySelector(".header-container");
+        this.__header = document.createElement("div");
+        this.__header.classList.add("header");
+        this.__container.appendChild(this.__header);
+
+        this.__date = document.createElement("span");
+        this.__date.classList.add("date");
+        this.__date.innerHTML = "MARCH 28, 2019";
+        
+        this.__header.appendChild(this.__date);
+        this.__title = document.createElement("h1");
+        this.__title.innerHTML = "Lambda Times";
+        this.__header.appendChild(this.__title);
+        
+        this.temp = document.createElement("span");
+        this.temp.classList.add("temp");
+        this.temp.innerHTML = "98°";
+        this.__header.appendChild(this.temp);
+        
+    }
+}
+
+function header(){
+    const __header = new Header();
+}
+
+header();
